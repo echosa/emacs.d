7c92fb05158deaa46ada2d96145521bee99badaa
Brian Zwahr <echosa@gmail.com>
2012-04-20 10:13:54 -0500

    Updating packages.

---
+++ elpa/paredit-20110508/paredit-pkg.el
@@ -0,0 +1,1 @@
+(define-package "paredit" "20110508" "minor mode for editing parentheses  -*- Mode: Emacs-Lisp -*- [source: darcs]" (quote nil))
---
+++ elpa/wrap-region-20120403/wrap-region-pkg.el
@@ -0,0 +1,1 @@
+(define-package "wrap-region" "20120403" "Wrap text with punctation or tag [source: github]" (quote nil))
