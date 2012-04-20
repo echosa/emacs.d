a53ac5a55e3aac8f5c4095065f7d132afb5c7ee0
Brian Zwahr <echosa@gmail.com>
2012-04-20 10:47:24 -0500

    Added window-number from elpa.

---
+++ elpa/window-number-1.0.1/window-number-autoloads.el
@@ -0,0 +1,18 @@
+;;; window-number-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("window-number-pkg.el" "window-number.el")
+;;;;;;  (20369 33663 827506))
+
+;;;***
+
+(provide 'window-number-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; window-number-autoloads.el ends here
---
+++ elpa/window-number-1.0.1/window-number-pkg.el
@@ -0,0 +1,1 @@
+(define-package "window-number" "1.0.1" "Jump to a window by number" (quote nil))
