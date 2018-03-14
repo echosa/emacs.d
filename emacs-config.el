e0d840f1b5cd81e4ec1c290e066a68e8db3b3da1
Brian Zwahr <echosa@icloud.com>
2018-03-14 10:55:31 -0500

    Make theme selection and loading a bit nicer.

--- emacs-config.el
+++ emacs-config.el
@@ -193,11 +193,16 @@ Argument REPLACE String used to replace the matched strings in the buffer.
       '((lambda nil
           (define-key reb-mode-map "\245" 'reb-query-replace-this-regxp))))
 
-(use-package solarized-theme
-  :if window-system
-  :ensure t
-  :config
-  (load-theme 'solarized-dark t))
+(let ((my-theme "tango-dark"))
+  (cond
+   ((string= my-theme "solarized-dark")
+    (use-package solarized-theme
+      :if window-system
+      :ensure t
+      :config
+      (load-theme 'solarized-dark t)))
+   ((string= my-theme "tango-dark")
+    (load-theme 'tango-dark t))))
 
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.tmp/" nil)))
 (setq auto-save-list-file-prefix "~/.emacs.tmp/.saves-")
