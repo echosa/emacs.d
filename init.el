6c05514ac0c7a8a87676743a82dfa0fa4bffbca8
Brian Zwahr <echosa@icloud.com>
2018-04-10 10:27:38 -0500

    Move customizations into custom.el.

--- init.el
+++ init.el
@@ -6,33 +6,10 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(geben-path-mappings
-   (quote
-    (("~/dev/chess.com" "/www/sites/chess.com/current"))))
- '(package-selected-packages
-   (quote
-    (use-package expand-region helm helm-open-github evil-goggles solarized-theme browse-at-remote geben php-mode exec-path-from-shell ag lua-mode cider flx-ido ido-vertical-mode evil evil-leader key-chord magit paredit pbcopy projectile))))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(evil-goggles-delete-face ((t (:inherit diff-removed))))
- '(evil-goggles-paste-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
- '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
- '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
- '(evil-goggles-yank-face ((t (:inherit diff-changed)))))
-
+(require 'cl) ;; php-cs-fixer
 (require 'org)
 (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-config.org"))
